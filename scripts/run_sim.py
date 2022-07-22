1a3a33220a9596237e40ae273cfc1c711bacc34d
Andy Park <chanhyup@andrew.cmu.edu>
2022-07-22 15:40:07 -0400

    wip

--- scripts/run_sim.py
+++ scripts/run_sim.py
@@ -13,20 +13,31 @@ from schwimmbad import MPIPool
 
 with open("../data/mask.pickle", "rb") as f:
     mask = pickle.load(f)
+    
+SIGMA_E = 0.25
+N_SOURCE = 12
+ARCMIN2RAD = 1 / 60 * (np.pi / 180)
+NOISE_CL = SIGMA_E ** 2 / N_SOURCE * ARCMIN2RAD ** 2
 
 
 def run_sims(seed):
     nside = 1024
-    rng = np.random.RandomState(seed=seed)
+    np.random.seed(seed)
     print("run", seed)
     gaussian_maps = dict()
-
+    
     for i in range(4):
         cl_fname = f"../data/cl_{i}_{i}.txt"
         _, cl = np.loadtxt(cl_fname)
+        noise_cl = NOISE_CL * np.ones_like(cl)
+        noise_map = hp.sphtfunc.synfast(
+            [noise_cl, noise_cl, noise_cl * 0, noise_cl, noise_cl * 0, noise_cl * 0], nside, new=True, pol=True
+        )
         gaussian_maps[i] = hp.sphtfunc.synfast(
             [cl, cl, cl * 0, cl, cl * 0, cl * 0], nside, new=True, pol=True
         )
+        gaussian_maps[i] += noise_map
+        gaussian_maps[i][:, ~mask[i]] = hp.UNSEEN
 
     Dl_nmt = dict()
     for i in range(4):
@@ -34,10 +45,10 @@ def run_sims(seed):
         f2_i = nmt.NmtField(mask[i], [gmi[1], gmi[2]])
         f2_j = nmt.NmtField(mask[i], [gmi[1], gmi[2]])
         Dl = nmt.compute_coupled_cell(f2_i, f2_j)
-        Dl_nmt[i] = Dl[0]
+        Dl_nmt[i] = Dl
         del f2_i, f2_j, Dl
 
-    fname = f"../sims/pcl_{seed}.pickle"
+    fname = f"../sims/pcl_{seed}_with_noise.pickle"
     with open(fname, "wb") as f:
         pickle.dump(Dl_nmt, f)
 
